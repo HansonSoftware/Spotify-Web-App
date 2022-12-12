2f741b723675da8d847f03f432ab89c34d7667e3
Hayden Hanson <hansonhayd3n@gmail.com>
2022-12-12 15:09:05 -0500

    Build Fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import '../dist/output.css'
+import '../output.css'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
